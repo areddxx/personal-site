1c8f6daa12140ec19b6d17d4d4c1fa732cea34f2
Austin Redd <hello@austinredd.com>
2020-09-13 21:08:08 -0600

    fix navigation on smaller screens

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,37 +1,45 @@
 <template>
   <nav>
-    <h1><NuxtLink class="link" to="/">Austin <span>Redd</span></NuxtLink></h1>
+    <h1>
+      <NuxtLink class="link" to="/">
+        Austin
+        <span>Redd</span>
+      </NuxtLink>
+    </h1>
     <ul class="navigation">
       <NuxtLink class="link" to="/work">Work</NuxtLink>
       <a href="https://github.com/areddxx" target="_blank">
-        <font-awesome-icon :icon="['fab', 'github']"/>
+        <font-awesome-icon :icon="['fab', 'github']" />
       </a>
     </ul>
   </nav>
 </template>
 
 <style lang="scss">
-  nav {
-    display: flex;
-    align-items: baseline;
-    margin: 30px 0;
-    width: 100%;
-    justify-content: space-between;
+nav {
+  display: flex;
+  align-items: baseline;
+  margin: 30px 0;
+  width: 100%;
+  justify-content: space-between;
+}
+h1 a {
+  color: #ddd;
+  font-weight: 700;
+  span {
+    opacity: 0.75;
   }
-  h1 a {
-    color: #DDD;
-    font-weight: 700;
-    span {
-      opacity: .75;
-    }
+}
+.navigation {
+  display: flex;
+}
+.navigation a {
+  text-decoration: none;
+  margin-right: 15px;
+  color: #dddddd;
+  &:last-of-type {
+    margin-right: 0;
+    color: #676e95;
   }
-  .navigation a {
-    text-decoration: none;
-    margin-right: 15px;
-    color: #dddddd;
-    &:last-of-type {
-      margin-right: 0;
-      color: #676E95;
-    }
-  }
+}
 </style>
