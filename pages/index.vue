1254828632c645ebde2a89bfd701491e30f590ab
Austin Redd <hello@austinredd.com>
2020-09-13 21:00:48 -0600

    update width and line height on home h1

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,11 @@ export default {}
 </script>
 
 <style lang="scss">
+  h1 {
+    max-width: 100%;
+    width: 700px;
+    line-height: 1.5;
+  }
   .contact-intro {
     font-size: 26px;
     margin-top: 45px;
